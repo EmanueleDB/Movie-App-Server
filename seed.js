5a7212b40724941ef43fbcbde81ab15341dd091a
LeleOi <leleoioioi@gmail.com>
2020-01-15 18:03:03 +0100

    bug fixing

--- seed.js
+++ seed.js
@@ -40,6 +40,7 @@ const data = [
 
 async function seed() {
   await mongoose.connect(config.get("db"), {
+    useCreateIndex: true,
     useUnifiedTopology: true,
     useNewUrlParser: true
   });
--- startup/db.js
+++ startup/db.js
@@ -5,6 +5,10 @@ const config = require("config");
 module.exports = function() {
   const db = config.get("db");
   mongoose
-    .connect(db)
+    .connect(db, {
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    })
     .then(() => winston.info(`Connected to ${db}...`));
 };
